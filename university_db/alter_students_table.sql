6fa14505842c67005dfaa10b2798f87eaac0b83b
KimberlyPalaca <palacakimberlyann_bsit@plmun.edu.ph>
2025-02-10 07:57:37 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 02

--- university_db/alter_students_table.sql
+++ university_db/alter_students_table.sql
@@ -1,6 +1,7 @@
+
 USE UniversityDB;
 
--- Add the Email column only if it doesn't exist
+
 ALTER TABLE Students ADD COLUMN Email VARCHAR(100);
 
 
--- university_db/create_students_table.sql
+++ university_db/create_students_table.sql
@@ -1,6 +1,10 @@
+
+USE UniversityDB;
+
 CREATE TABLE IF NOT EXISTS Students (
     StudentID INT PRIMARY KEY AUTO_INCREMENT,
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
-    EnrollmentDate DATE
+    EnrollmentDate DATE,
+    Email VARCHAR(100) 
 );
