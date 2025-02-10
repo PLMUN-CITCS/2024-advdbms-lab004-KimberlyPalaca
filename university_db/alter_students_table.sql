73d030904430728c9225a81089278194f6d71103
KimberlyPalaca <palacakimberlyann_bsit@plmun.edu.ph>
2025-02-10 08:01:25 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 02

--- university_db/alter_students_table.sql
+++ university_db/alter_students_table.sql
@@ -1,8 +1,10 @@
-
 USE UniversityDB;
 
+SET @col_exists = (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+                   WHERE TABLE_NAME = 'Students' AND COLUMN_NAME = 'Email');
 
-ALTER TABLE Students ADD COLUMN Email VARCHAR(100);
+SET @sql = IF(@col_exists = 0, 'ALTER TABLE Students ADD COLUMN Email VARCHAR(100);', 'SELECT "Column already exists";');
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
-
-DESCRIBE Students;
